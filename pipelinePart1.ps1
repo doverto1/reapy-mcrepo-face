7c83fdbcd2bfa17065b60823daf8e0a3d6c6adb1
Don Overton <doverto1@asu.edu>
2021-06-21 20:52:16 -0700

    pipelinePart 1

--- pipelinePart1.ps1
+++ pipelinePart1.ps1
@@ -1,1 +1,7 @@
-Get-ChildItem Z:\Desktop\reapy-mcrepo-face\Company\*.txt -Recurse | Remove-Item -confirm
+# Get-ChildItem Z:\Desktop\reapy-mcrepo-face\content\*.txt -Recurse | Remove-Item -WhatIf
+
+# Get-ChildItem Z:\Desktop\reapy-mcrepo-face\Company\*.txt -Recurse | Remove-Item -confirm
+
+#Get-EventLog -LogName Application | Out-File Z:\Desktop\reapy-mcrepo-face\app.txt
+
+#Get-Service BITS | Stop-Service -PassThru
