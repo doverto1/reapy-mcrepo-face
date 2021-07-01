26a38531b2618af19b33c51577b8c6244f2b6003
Don Overton <doverto1@asu.edu>
2021-06-30 19:30:02 -0700

    added new ps files.

---
+++ remote.ps1
@@ -0,0 +1,5 @@
+$pw = convertto-securestring -AsPlainText -Force -String "E=c@&fnm!X5yNirz=C9zy!85Pg8l;Qls"
+ $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist "Administrator",$pw
+ $session = new-pssession -computername ec2-35-80-21-193.us-west-2.compute.amazonaws.com -credential $cred
+ Invoke-Command -Session $session {Get-ChildItem C:\Users\Administrator}
+#  Invoke-Command -Session $session {$h | where {$_.InstallBy -ne "NTAUTHORITY\SYSTEM"}}
